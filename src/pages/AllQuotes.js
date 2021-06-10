403e300c03688cbaa258b97f8ec3c898e6bdb042
Suraj Singh <surajsingh220698@outlook.com>
2021-06-10 21:59:13 +0530

    Add dummy data and comment section on new quote section.

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great" },
+];
+
 const AllQuote = () => {
-  return <h1>AllQuote Page</h1>;
+  return <QuoteList quotes={DUMMY_DATA} />;
 };
 
 export default AllQuote;
