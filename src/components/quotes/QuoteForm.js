4e8660ae4185ba2216d78298a29fd890158ade48
Suraj Singh <surajsingh220698@outlook.com>
2021-06-11 09:26:08 +0530

    Implementing programmatic navigation and prevent unwanted route transition with the prompt component.

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,13 @@
-import { useRef } from "react";
+import { Prompt } from "react-router-dom";
+import { useState, useRef, Fragment } from "react";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +22,44 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishFocusHandler = () => setIsEntering(false);
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusHandler = () => setIsEntering(true);
+
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={() =>
+          "Are you sure you want to leave? All your data would be lost!"
+        }
+      />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn">Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
-  const addQuoteHandler = (quoteData) => console.log(quoteData);
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push("/quotes");
+  };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
